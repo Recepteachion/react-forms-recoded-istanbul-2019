6ac9bdf2caed0c77b79b057f78ccd594941aad8f
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-02 17:57:26 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -47,5 +47,15 @@ class Form extends React.Component {
     )
   }
 }
+class DisplayData extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.formData.firstName}</h1>
+        <h1>{this.props.formData.lastName}</h1>
+      </div>
+    )
+  }
+}
  
 export default ParentComponent;
