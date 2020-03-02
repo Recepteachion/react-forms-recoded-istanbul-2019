18020bc05eff2f1fd2adce0232d536e21caae30e
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-02 17:57:13 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Form from './Form'
-import DisplayData from './DisplayData'
+
  
 class ParentComponent extends React.Component {
   state = {
@@ -22,6 +21,28 @@ class ParentComponent extends React.Component {
           handleChange={this.handleChange}
         />
         <DisplayData formData={this.state} />
+      </div>
+    )
+  }
+}
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName} />
+        </form>
       </div>
     )
   }
